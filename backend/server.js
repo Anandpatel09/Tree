2e1bc2c576d23a9cfecdde100e3e88bb9f4d32c9
Anandpatel09 <anandbca2021@gmail.com>
2026-02-22 23:59:01 +0530

    adde api call functionality in login and signup page

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app=express()
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes)
+app.use("/auth", authRoutes)
 
 const Port=process.env.PORT || 5000
 app.listen(Port,()=>console.log(`server is running on port ${Port}`))
